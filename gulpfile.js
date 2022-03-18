70d22d11ac8df6c00f8d54a51dc436d97be8ca4d
Ben Teegarden <benteegarden@gmail.com>
2022-03-17 23:45:30 -0700

    Clean up gulpfile. Rerun gulp. Remove unnecessary files from build directory.

--- gulpfile.js
+++ gulpfile.js
@@ -21,73 +21,43 @@ var gulp = require('gulp'),
 
 // SCSS/CSS
 gulp.task('build-css', function() {
-  // gulp.src('./css/portfolio.css')
-  //   // .pipe(sourcemaps.init())
-  //   // .pipe(sass({
-  //   //   includePaths: ['./node_modules/foundation-sites/scss']
-  //   // }).on('error', sass.logError))
-  //   .pipe(autoprefixer({
-  //     browsers: ['last 25 versions']
-  //   }))
-  //   .pipe(minifycss())
-  //   .pipe(rename({suffix: '.min'}))
-  //   // .pipe(sourcemaps.write('.'))
-  //   .pipe(gulp.dest('./build/css'));
-
-  gulp.src(['./css/portfolio.css', './css/portfolio--mk.css'])
-    .pipe(autoprefixer({
-      browsers: ['last 25 versions']
-    }))
+  return gulp
+    .src(['./css/portfolio.css', './css/portfolio--mk.css', './css/portfolio--blt.css'])
+    .pipe(
+      autoprefixer({
+        Browserslist: ['last 25 versions']
+      })
+    )
     .pipe(minifycss())
-    .pipe(concat('portfolio--mk.min.css'))
-    .pipe(gulp.dest('./build/css'));
-
-  gulp.src(['./css/portfolio.css', './css/portfolio--blt.css'])
-    .pipe(autoprefixer({
-      browsers: ['last 25 versions']
-    }))
-    .pipe(minifycss())
-    .pipe(concat('portfolio--blt.min.css'))
     .pipe(gulp.dest('./build/css'));
 });
 
-// JS
-var jspaths = [
-  './js/app.js',
-  './js/provider--config.js',
-  './js/config.js',
-  './js/factory--image-preloader.js',
-  './js/factory--rows.js',
-];
-
+// Task: Build JS.
 gulp.task('build-js', function() {
-  gulp.src(jspaths)
+  return gulp
+    .src([
+      './js/app.js',
+      './js/provider--config.js',
+      './js/config.js',
+      './js/factory--image-preloader.js',
+      './js/factory--rows.js',
+    ])
     .pipe(concat('app-concat.js'))
     .pipe(closure({angular: true}))
     .pipe(ngannotate())
-    .pipe(gulp.dest('./build/js'))
     .pipe(uglify({mangle: false}))
     .pipe(minifyjs())
-    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/js'));
-
-  gulp.src('./js/libraries/flexslider/angular-flexslider.js')
-    .pipe(concat('angular-flexslider.js'))
-    .pipe(closure({angular: true}))
-    .pipe(ngannotate())
-    .pipe(gulp.dest('./build/js/libraries/flexslider'))
-    .pipe(uglify({mangle: false}))
-    .pipe(minifyjs())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./build/js/libraries/flexslider'));
 });
 
-gulp.task('build', ['build-css', 'build-js']);
+// Task: Build both CSS and JS.
+gulp.task('build', gulp.series('build-css', 'build-js'));
 
-// Watchers
+// Task: Watch both CSS and JS.
 gulp.task('watch', function() {
-  gulp.watch('./css/**/*.css', ['build-css']);
-  gulp.watch('./js/**/*.js', ['build-js']);
+  gulp.watch('./css/**/*.css', gulp.series('build-css'));
+  gulp.watch('./js/**/*.js', gulp.series('build-js'));
 });
 
-gulp.task('default', ['build-css', 'build-js', 'watch']);
+// Task: Default Task.
+gulp.task('default', gulp.series('build-css', 'build-js', 'watch'));
